ecf250c2191c14bbd94ce05f68d59ee58b06134b
gggg2614 <839857519@qq.com>
2022-08-09 15:50:04 +0800

    admin done

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,20 +1,25 @@
+const AdminUser = require('../../models/AdminUser')
+
 module.exports = app => {
     const express = require('express')
+    const assert = require('http-assert')
+    const jwt = require('jsonwebtoken')
+    const AdminUser = require('../../models/AdminUser')
+
     const router = express.Router({
         mergeParams: true
     })
-
-
+    //创建资源
     router.post('/', async (req, res) => {
         const model = await req.Model.create(req.body)
         res.send(model)
     })
-
+    //更新资源
     router.put('/:id', async (req, res) => {
         const model = await req.Model.findByIdAndUpdate(req.params.id, req.body)
         res.send(model)
     })
-    //删除
+    //删除资源
     router.delete('/:id', async (req, res) => {
         await req.Model.findByIdAndDelete(req.params.id, req.body)
         res.send({ sucess: true })
@@ -33,18 +38,39 @@ module.exports = app => {
         const model = await req.Model.findById(req.params.id)
         res.send(model)
     })
+    //登录校验中间件
+    const authMiddleware = require('../../middleware/auth')
+
+    const resourceMiddleware = require('../../middleware/resource')
     //通用CURD接口
-    app.use('/admin/api/rest/:resource', async (req, res, next) => {
-        const modelName = require('inflection').classify(req.params.resource)
-        req.Model = require(`../../models/${modelName}`)
-        next()
-    }, router)
+    app.use('/admin/api/rest/:resource', authMiddleware(), resourceMiddleware(), router)
     //解析上传图片
     const multer = require('multer')
     const upload = multer({ dest: __dirname + '/../../uploads' })
-    app.post('/admin/api/upload', upload.single('file'), async (req, res, next) => {
+    app.post('/admin/api/upload', authMiddleware(), upload.single('file'), async (req, res, next) => {
         const file = req.file
-        file.url=`http://localhost:3000/uploads/${file.filename}`
+        file.url = `http://localhost:3000/uploads/${file.filename}`
         res.send(file)
     })
+    //根据用户名找用户
+    app.post('/admin/api/login', async (req, res) => {
+        const { username, password } = req.body
+        const user = await AdminUser.findOne({ username }).select('+password')
+        assert(user, 422, '用户不存在')
+
+        //校验密码
+        const isValid = require('bcryptjs').compareSync(password, user.password)
+        assert(isValid, 422, '密码错误')
+
+        //返回token
+        const token = jwt.sign({ id: user._id }, app.get('secret'))
+        res.send({ token })
+    })
+    //错误处理
+    app.use(async (err, req, res, next) => {
+        res.status(err.statusCode || 500).send({
+            message: err.message
+        })
+    }
+    )
 }
