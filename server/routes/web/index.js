44024c909e285d01ec99ff4bf4bc3a03d775e082
gggg2614 <839857519@qq.com>
2022-08-12 22:01:50 +0800

    heroListdone

--- server/routes/web/index.js
+++ server/routes/web/index.js
@@ -3,7 +3,8 @@ module.exports = app => {
     const mongoose = require('mongoose')
     const Article = mongoose.model('Article')
     const Category = mongoose.model('Category')
-
+    const Hero = mongoose.model('Hero')
+    //导入新闻数据
     router.get('/news/init', async (req, res) => {
         const parent = await Category.findOne({
             name: '新闻分类'
@@ -23,15 +24,8 @@ module.exports = app => {
         await Article.insertMany(newsList)
         res.send(newsList);
     })
+    //新闻列表
     router.get('/news/list', async (req, res) => {
-        // const parent = await Category.findOne({
-        //     name: '新闻分类'
-        // }).populate({
-        //     path: 'children',
-        //     populate: {
-        //         path: 'newsList'
-        //     }
-        // }).lean()
         const parent = await Category.findOne({
             name: '新闻分类'
         })
@@ -69,6 +63,52 @@ module.exports = app => {
         })
         res.send(cats)
     })
+    //导入英雄数据
+    router.get('/heroes/init', async (req, res) => {
+        await Hero.deleteMany({})
+        const rawData = require('./hero.json')
+        for (let cat of rawData) {
+            if (cat.name === '热门') {
+                continue
+            }
+            //找到当前分类在数据库中对应的数据
+            const category = await Category.findOne({
+                name: cat.name
+            })
+            cat.heroes = cat.heroes.map(hero => {
+                hero.categories = [category]
+                return hero
+            })
+            //录入英雄  
+            await Hero.insertMany(cat.heroes)
+        }
+        res.send(rawData)
+    })
+    //英雄列表
+    router.get('/heroes/list', async (req, res) => {
+        const parent = await Category.findOne({
+            name: '英雄分类'
+        })
+        const cats = await Category.aggregate([
+            { $match: { parent: parent._id } },
+            {
+                $lookup: {
+                    from: 'heroes',
+                    localField: '_id',
+                    foreignField: 'categories',
+                    as: 'heroList'
+                },
+            },
+        ])
+        const subCats = cats.map(v => v._id)
+        cats.unshift({
+            name: '热门',
+            heroList: await Hero.find().where({
+                categories: { $in: subCats }
+            }).limit(10).lean()
+        })
 
+        res.send(cats)
+    })
     app.use('/web/api', router)
 }
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,47 +1,57 @@
 <template>
-  <div>
+<div>
     <swiper :options="swiperOption" ref="mySwiper">
-      <swiper-slide>
-        <img class="w-100" src="@/assets/images/1.jpeg" alt="" />
-      </swiper-slide>
-      <swiper-slide>
-        <img class="w-100" src="@/assets/images/2.jpeg" alt="" />
-      </swiper-slide>
-      <swiper-slide>
-        <img class="w-100" src="@/assets/images/3.jpeg" alt="" />
-      </swiper-slide>
-      <div
-        class="swiper-pagination pagination-home text-right px-3 pb-2"
-        slot="pagination"
-      ></div>
+        <swiper-slide>
+            <img class="w-100" src="@/assets/images/1.jpeg" alt="" />
+        </swiper-slide>
+        <swiper-slide>
+            <img class="w-100" src="@/assets/images/2.jpeg" alt="" />
+        </swiper-slide>
+        <swiper-slide>
+            <img class="w-100" src="@/assets/images/3.jpeg" alt="" />
+        </swiper-slide>
+        <div class="swiper-pagination pagination-home text-right px-3 pb-2" slot="pagination"></div>
     </swiper>
     <!-- end of swiper -->
     <div class="nav-icons bg-white mt-3 text-center pt-3 text-dark-1">
-      <div class="d-flex flex-wrap">
-        <div class="nav-item mb-3" v-for="n in 12" :key="n">
-          <i class="sprite sprite-news"></i>
-          <div class="py-1">爆料站</div>
+        <div class="d-flex flex-wrap">
+            <div class="nav-item mb-3" v-for="n in 12" :key="n">
+                <i class="sprite sprite-news"></i>
+                <div class="py-1">爆料站</div>
+            </div>
         </div>
-      </div>
-      <div class="bg-light py-2 fs-sm">
-        <i class="sprite sprite-arrow mr-1"></i>
-        <span>收起</span>
-      </div>
+        <div class="bg-light py-2 fs-sm">
+            <i class="sprite sprite-arrow mr-1"></i>
+            <span>收起</span>
+        </div>
     </div>
     <!-- end of nav icons -->
 
     <m-list-card icon="Menu" title="新闻资讯" :categories="newsCats">
-      <template #items="{ category }">
-        <div class="py-2 fs-lg d-flex" v-for="(news, i) in category.newsList" :key="i">
-          <span class="text-info">[{{ news.categoryName }}]</span>
-          <span class="px-2">|</span>
-          <span class="flex-1 text-dark-1 text-ellipsis pr-2">{{ news.title }}</span>
-          <span class="text-grey-1 fs-sm">{{ news.createdAt | date}}</span>
-        </div>
-      </template>
+        <template #items="{ category }">
+            <div class="py-2 fs-lg d-flex" v-for="(news, i) in category.newsList" :key="i">
+                <span class="text-info">[{{ news.categoryName }}]</span>
+                <span class="px-2">|</span>
+                <span class="flex-1 text-dark-1 text-ellipsis pr-2">{{
+            news.title
+          }}</span>
+                <span class="text-grey-1 fs-sm">{{ news.createdAt | date }}</span>
+            </div>
+        </template>
     </m-list-card>
 
-    <m-card icon="Menu" title="英雄列表"></m-card>
+    <m-list-card icon="Menu" title="英雄列表" :categories="heroCats">
+        <template #items="{ category }">
+          <div class="d-flex flex-wrap" style="margin:0 -0.5rem">
+            <div class="p-2 text-center" style="width:20%" v-for="(hero, i) in category.heroList" :key="i">
+              <img class="w-100" :src="hero.avatar">
+              <div>{{hero.name}}</div>
+            </div>
+          </div>
+        </template>
+    </m-list-card>
+
+    <m-card icon="Menu" title="精彩视频"></m-card>
     <p>aaa</p>
     <p>aaa</p>
     <p>aaa</p>
@@ -50,37 +60,43 @@
     <p>aaa</p>
     <p>aaa</p>
     <p>aaa</p>
-  </div>
+</div>
 </template>
 
 <script>
-import dayjs from 'dayjs'
+import dayjs from "dayjs";
 export default {
-  filters: {
-    date(val){
-      return dayjs(val).format('MM/DD')
-    }
-  },
-  name: "carrousel",
-  data() {
-    return {
-      swiperOption: {
-        pagination: {
-          el: ".pagination-home",
+    filters: {
+        date(val) {
+            return dayjs(val).format("MM/DD");
         },
-      },
-      newsCats: [],
-    };
-  },
-  methods: {
-    async fetchNewsCats() {
-      const res = await this.$http.get("/news/list");
-      this.newsCats = res.data;
     },
-  },
-  created() {
-    this.fetchNewsCats();
-  },
+    name: "carrousel",
+    data() {
+        return {
+            swiperOption: {
+                pagination: {
+                    el: ".pagination-home",
+                },
+            },
+            newsCats: [],
+            heroCats: [],
+        };
+    },
+    methods: {
+        async fetchNewsCats() {
+            const res = await this.$http.get("/news/list");
+            this.newsCats = res.data;
+        },
+        async fetchHeroesCats() {
+            const res = await this.$http.get("/heroes/list");
+            this.heroCats = res.data;
+        },
+    },
+    created() {
+        this.fetchNewsCats();
+        this.fetchHeroesCats();
+    },
 };
 </script>
 
@@ -88,25 +104,28 @@ export default {
 @import "@/assets/scss/variables.scss";
 
 .pagination-home {
-  .swiper-pagination-bullet {
-    opacity: 1;
-    border-radius: 0.1538rem;
-    background: map-get($colors, "white");
+    .swiper-pagination-bullet {
+        opacity: 1;
+        border-radius: 0.1538rem;
+        background: map-get($colors, "white");
 
-    &.swiper-pagination-bullet-active {
-      background: map-get($colors, "info");
+        &.swiper-pagination-bullet-active {
+            background: map-get($colors, "info");
+        }
     }
-  }
 }
+
 .nav-icons {
-  border-top: 1px solid $border-color;
-  border-bottom: 1px solid $border-color;
-  .nav-item {
-    width: 25%;
-    border-right: 1px solid $border-color;
-    &:nth-child(4n) {
-      border-right: none;
+    border-top: 1px solid $border-color;
+    border-bottom: 1px solid $border-color;
+
+    .nav-item {
+        width: 25%;
+        border-right: 1px solid $border-color;
+
+        &:nth-child(4n) {
+            border-right: none;
+        }
     }
-  }
 }
 </style>
